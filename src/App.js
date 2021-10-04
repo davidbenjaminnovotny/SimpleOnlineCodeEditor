5f0c9b78d9a9527019668ad5466e102f0604a488
David Novotny <novotnydavid3@gmail.com>
2021-10-04 18:00:04 +0200

    codemirror set up

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@
 import React from 'react'
 import './App.css';
 
+
 import Editor from './components/Editor'
 
 function App() {
+  const xml = {
+    title:"Html",
+    mode: "xml"
+  }
+  const css = {
+    title:"Css",
+    mode: "css"
+  }
+  const javascript = {
+    title:"Javascript",
+    mode: "javascript"
+  }
+  
   return (
     <div className="App">
       <div className="pane top-pane">
-        <Editor className="editorComponent"/>
-        <Editor className="editorComponent"/>
-        <Editor className="editorComponent"/>
+        <Editor className="editorComponent" title={xml.title} mode={xml.mode}/>
+        <Editor className="editorComponent" title={css.title} mode={css.mode}/>
+        <Editor className="editorComponent" title={javascript.title} mode={javascript.mode}/>
       </div>
       <div className="pane bottom-pane">
      
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,41 +1,55 @@
+
 import React from 'react'
 import '../App.css'
 
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 
-import {Controlled as CodeMirror} from 'react-codemirror2'
 
 
-function Editor() {
+
+
+import { Controlled as CodeMirror } from 'react-codemirror2'
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/css/css');
+
+function Editor({title,mode}) {
+    const [code, setCode] = React.useState('');
+   
+
     const options = {
-       
-        mode: 'material',
+        mode: mode,
+        theme: 'material',
         lineNumbers: true,
         lineWrapping: true,
-        showCursorWhenSelecting: true,   
-        // allowDropFileTypes: ['image/png']
-        // hintOptions related keys
+        showCursorWhenSelecting: true,
+        
         alignWithWord: true,
         spellcheck: true,
-       
-      };
-  return (
-    <div className="Editor">
-        <div className="EditorHeader">
-            <button className="EditorHeader_Resize">Resize</button>
-        </div>
-<CodeMirror
-    options={options}
-  onBeforeChange={(editor, data, value) => {
-    console.log("hello")
-  }}
-  onChange={(editor, data, value) => {
-  }}
-/>
 
-    </div>
-  );
+    };
+
+    return (
+        <div className="Editor">
+            <div className="EditorHeader">
+            <h1 className="EditorHeader_Title">{title}</h1>
+                <button className="EditorHeader_Resize">Resize</button>
+            
+            </div>
+            <CodeMirror
+                value={code}
+                options={options}
+                onBeforeChange={(editor, data, value) => {
+                    setCode(value);
+                }}
+                onChange={(editor, data, value) => {
+                }}
+            />
+
+        </div>
+    );
 }
 
 export default Editor;
